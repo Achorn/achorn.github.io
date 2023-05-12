fc31ceda0ea33802defd5d9c18f90dc64abaae7b
achorn <joshuaachorn@gmail.com>
2023-05-11 17:06:58 -0700

    updated music image and updated all descriptions

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,5 +1,7 @@
 import "./Main.css";
 import HeroSection from "./hero/Hero";
+import JoshWithGuitar from "../assets/josh-with-guitar.jpg";
+
 import Topic from "./topic/Topic";
 import SelectableUnderline from "../components/functional/Selectable-underline-button";
 import { useState } from "react";
@@ -29,9 +31,10 @@ function Main() {
         id="music"
         child={
           <Topic
+            image={JoshWithGuitar}
             title={"MUSIC"}
             description={
-              "My first(and longest standing) passion.  Expressing myself with music Expressing myself with music Expressing myself with music Expressing myself with music Expressing myself with musicExpressing myself with music"
+              "My first and longest lasting passion! Music has been a great way for me to express myself through writing and performing. Being on stage and sharing my music is always an uplifting experience but at the heart of it all, I write for myself. Most of the time its me and my guitar, but I always enjoy playing with friends."
             }
           />
         }
@@ -42,7 +45,7 @@ function Main() {
           <Topic
             title={"CODING"}
             description={
-              "First thing to know about me when it comes to coding. I love clean readable code.  to coding. I love clean readable code.  to coding. I love clean readable code.  to coding. I love clean readable code. "
+              "The cross section between computer science and art. As someone who loves both creating, and solving sudoku puzzles, coding scratches both of these itches. I'm primarily a front end developer with some experience in backend. I enjoy writing clean, readable and maintainable code, while using the MVVM pattern to separate the business logic, models, and the views. Languages and frameworks I’ve used professionally are Flutter, Dart, React, Javascript."
             }
           />
         }
@@ -53,7 +56,7 @@ function Main() {
           <Topic
             title={"DESIGN"}
             description={
-              "Along with music, historically, I've mostly been focussed on art. drawing, painting, anything where im physically using my hands to create something with colors or materials. hands to create something with colors or materials hands to create something with colors or "
+              "I love working with my hands, whether it be, drawing, painting, analog, digital, building . restoring, etc. This topic is pretty broad so I'm going to be dumping a hodgepodge of my interest in this link. I’ll keep the professional pieces at the top though ;)"
             }
           />
         }
@@ -61,7 +64,6 @@ function Main() {
     </header>
   );
 }
-// var selectedId = "coding";
 
 function NavBar({ handleClick, selectedId }) {
   return (
--- src/main/topic/Topic.js
+++ src/main/topic/Topic.js
@@ -2,24 +2,24 @@ import "./Topic.css";
 import design from "../../assets/design.jpg";
 import { ReactComponent as Acorn } from "../../assets/acorn.svg";
 import { FiChevronRight } from "react-icons/fi";
-import UnderConstruction from "../../components/display/under-construction/Under-construction";
-export default function Topic({ title, description, icon }) {
+
+export default function Topic({ image, title, description, icon }) {
   return (
     <div className="Topic-container">
       <div className="Topic-image-container">
-        <img src={design} className="Hero-image" alt="logo" />
+        <img
+          src={image != null ? image : design}
+          className="Hero-image"
+          alt="logo"
+        />
       </div>
       <div className="Topic-words-container">
         <h2 className="Topic-title">{title}</h2>
-        <div className="Uc-container">
-          <div className="Topic-uc">
-            <UnderConstruction description="Description + Links" />
-          </div>
 
-          <div className="Description-container">
-            <h3 className="Topic-description">{description}</h3>
-          </div>
+        <div className="Description-container">
+          <h3 className="Topic-description">{description}</h3>
         </div>
+
         <div className="Learn-more-container">
           <Acorn className="Topic-icon" />
 
