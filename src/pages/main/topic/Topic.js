e0d250c1dc505de814fc6d5e865f9e91209ae7d7
achorn <joshuaachorn@gmail.com>
2023-10-18 16:31:45 -0700

    added animation to topic descriptions

--- src/pages/main/topic/Topic.js
+++ src/pages/main/topic/Topic.js
@@ -2,8 +2,23 @@ import "./Topic.css";
 import design from "../../../assets/design.jpg";
 import { FiChevronRight } from "react-icons/fi";
 import { NavLink as Link } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 export default function Topic({ image, title, description, Icon }) {
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const buttonRef = useRef();
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+    observer.observe(titleRef.current);
+    observer.observe(descriptionRef.current);
+    observer.observe(buttonRef.current);
+  }, []);
   return (
     <div className="Topic-container">
       <div className="Topic-image-container">
@@ -15,13 +30,15 @@ export default function Topic({ image, title, description, Icon }) {
         />
       </div>
       <div className="Topic-words-container">
-        <h2 className="Topic-title">{title.toUpperCase()}</h2>
+        <h2 ref={titleRef} className="Topic-title hidden">
+          {title.toUpperCase()}
+        </h2>
 
-        <div className="Description-container">
+        <div ref={descriptionRef} className="Description-container hidden">
           <h3 className="Topic-description">{description}</h3>
         </div>
         <Link to={`/${title}`}>
-          <div className="Learn-more-container">
+          <div ref={buttonRef} className="Learn-more-container hidden">
             <FiChevronRight
               color="#1f2425"
               size={40}
