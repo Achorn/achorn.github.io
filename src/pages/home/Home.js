fb3be15a6172cb96d92bbf830e73bdba4a8dc2df
achorn <joshuaachorn@gmail.com>
2024-03-05 10:19:44 -0800

    added correct sizing and order for large and small screens

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,33 +1,38 @@
 import React from "react";
 import FlexBetween from "../../components/FlexBetween";
-import { Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 
 const Home = () => {
   return (
     <Container>
       {/* Nav section */}
       <FlexBetween>
-        <div>Joshua Achorn</div>
-        <FlexBetween gap={"10px"}>
-          <div>About</div>
-          <div>Projects</div>
-          {/* <div>Blog</div> */}
-          <div>Contact</div>
+        <Typography sx={{ color: "text.secondary" }}>Joshua Achorn</Typography>
+        <FlexBetween gap={"20px"}>
+          <Typography sx={{ color: "text.secondary" }}>About</Typography>
+
+          <Typography sx={{ color: "text.secondary" }}>Projects</Typography>
+          <Typography sx={{ color: "text.secondary" }}>Contact</Typography>
+          {/* <Typography sx={{ color: "text.secondary" }}>Blog</Typography> */}
         </FlexBetween>
       </FlexBetween>
 
       {/* hero section */}
-      <div>HERO</div>
+      <Box sx={{ mt: "50px" }} />
       <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <div>
-            <Typography variant="h3">Title </Typography>
-            <Typography>description </Typography>
-            <Button>View Linkedin</Button>
-            <Button>View Github</Button>
-          </div>
+        <Grid item order={{ xs: 2, md: 1 }} xs={12} md={8} lg={8} align="left">
+          <Typography variant="h4" align="left" sx={{ mb: "10px" }}>
+            Hello! I'm Joshua, a full stack developer based in SF.
+          </Typography>
+          <Typography align="left" sx={{ mb: "10px" }}>
+            I've had 3 years of working at startups as a founding team member.
+          </Typography>
+          <Button variant="contained">View Linkedin</Button>
+          <Button variant="outlined" sx={{ ml: "10px" }}>
+            View Github
+          </Button>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={4} lg={4} order={{ xs: 1, md: 2 }}>
           Photo
         </Grid>
       </Grid>
